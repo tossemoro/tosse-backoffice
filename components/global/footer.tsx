93ed79a00f7d4a2d14880d56884104aedc747e72
mrvin100 <vincentyoumssi@gmail.com>
2024-12-01 23:29:25 +0100

    changement de la balise div par footer pour optimiser le SEO, configuration du dashboard avec le background du sidebar et mise a jour automatique de l'anne de copyrighting

--- components/global/footer.tsx
+++ components/global/footer.tsx
@@ -2,6 +2,6 @@ import Link from "next/link";
 
 export function Footer() {
     return (
-        <div className="text-[#FFFFFF] py-4 text-center bg-[#303030] mt-4">© copyright @ 2024 <Link href="/" className="border-b-2 hover:text-[#FF9D00]">Tossé</Link> | Allrightsreserved.</div>
+        <footer className="py-4 text-center bg-sidebar border-t">© copyright @{new Date().getFullYear()} <Link href="/" className="border-b-2 hover:text-[#FF9D00]">Tossé</Link> | All rights reserved.</footer>
     )
 }
