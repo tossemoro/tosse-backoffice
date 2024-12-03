f84429ab401135922a5c42727fe0f0a1dc0dc75c
Hassan <hassanmahamatdogo@gmail.com>
2024-12-03 09:19:46 +0100

    Ajouter de la formulaire

--- app/(dasboard)/layout.tsx
+++ app/(dasboard)/layout.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-// import { Sidebar, Footer, Header } from "@/components/global";
 import { SidebarProviders } from "@/components/providers/sidebar.provider";
 
 export const metadata: Metadata = {
--- components/sign-up/signupform.tsx
+++ components/sign-up/signupform.tsx
@@ -1,24 +1,50 @@
-import Link from "next/link"
 import { TypographyH4 } from "../ui/typographies"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import * as React from "react"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
 export function SignUpForm() {
-    return(
-        <div className="flex items-center w-full border border-gray-400 bg-white rounded">
-            <div className="flex items-center justify-betwen">
-                <TypographyH4>
-                    Inscription
-                </TypographyH4>
-                <div className="flex items-center gap-2">
-                    <TypographyH4>
-                        Tu as déjà un compte ?
-                    </TypographyH4>
-                    <TypographyH4>
-                        <Link href="/connsxion">Connexion</Link>
-                    </TypographyH4>
-                </div>
-            </div>
-            <form>
-                
+    return (
+        <div className="max-auto">
+            <TypographyH4 className="bg-primary text-white">
+                Inscription
+            </TypographyH4>
+            <form className="pt-8 pb-5 space-y-4 w-[100%] border border-gray-400 bg-white rounded">
+                <Input
+                    placeholder="Nom"
+                    type="text"
+                    id="email"
+                />
+                <Input
+                    placeholder="hassan@gmail.com"
+                    type="email"
+                    id="email"
+                />
+                <Input
+                    placeholder="Mot de passe"
+                    type="password"
+                    id="password"
+                />
+                <Select>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Administrateur" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="administrateur">Administrateur</SelectItem>
+                            <SelectItem value="stagaire">Stagaire</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <Button type="submit">S&apos;inscrire</Button>
             </form>
         </div>
     )
