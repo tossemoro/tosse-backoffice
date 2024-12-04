340fe686655ec4ac8c935db80a6862a7fab733f3
Hassan <hassanmahamatdogo@gmail.com>
2024-12-04 10:26:15 +0100

    Modification du composant sidebar

--- components/global/app-sidebar.tsx
+++ components/global/app-sidebar.tsx
@@ -1,18 +1,14 @@
-import Link from "next/link";
-import { BadgePoundSterling, ChevronUp, User2 } from "lucide-react";
+import { BadgePoundSterling, Inbox } from "lucide-react"
 
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+} from "@/components/ui/sidebar"
 
 const items = [
   {
@@ -22,26 +18,25 @@ const items = [
   },
   {
     title: "Messagerie",
-    url: "/messagerie",
-    icon: null, // Vous pouvez ajouter une icône ici si besoin
-  },
-];
+    url: "/inbox",
+    icon: Inbox,
+  }
+]
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    {/* Utilisation de Link pour la navigation */}
-                    <Link href={item.url}>
+                    <a href={item.url}>
+                      <item.icon />
                       <span>{item.title}</span>
-                    </Link>
+                    </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
@@ -49,34 +44,6 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton>
-                  <User2 /> Username (hassan)
-                  <ChevronUp className="ml-auto" />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                side="top"
-                className="w-[--radix-popper-anchor-width]"
-              >
-                <DropdownMenuItem>
-                  <span>Account</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <span>Billing</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <span>Sign out</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
     </Sidebar>
-  );
+  )
 }
